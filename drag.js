be88eb2cd0e553f9a1d2c4912c36ccda1c08c7e4
JaneGaoo <109293140@qq.com>
2016-12-16 13:18:31 +0800

    change2 drag.js

--- drag.js
+++ drag.js
@@ -2,7 +2,10 @@
  * Created by Administrator on 2016/12/16.
  */
 
- function drag(){
-	 
-	 
+ function drag(id){
+	var obj = document.getElementById(id);
+	
+	obj.onmousedown = function(){
+		
+	}
  }
