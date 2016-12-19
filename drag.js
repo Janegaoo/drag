357c472bfc99f1c547daefa1c9bbb2f8894595eb
JaneGaoo <109293140@qq.com>
2016-12-19 16:26:01 +0800

    change drag.js

--- drag.js
+++ drag.js
@@ -6,7 +6,9 @@
 	var obj = document.getElementById(id);
 	var disX = 0;
 	var disY = 0;
-	obj.onmousedown = function(){
+	obj.onmousedown = function(ev){
+		disX = ev.pageX - obj.offsetLeft;
+		disY = ev.pageY - obj.offsetTop;
 		
 	}
  }
